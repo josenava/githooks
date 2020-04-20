8787cf1315c5c9900741649b22bbd26f8cd920f3
Jose Navarrete <janavarretecristino@gmail.com>
2020-04-20 14:52:03 +0200

    fix parameter order

--- python-pylintscore.sh
+++ python-pylintscore.sh
@@ -7,8 +7,8 @@ greaterThan() {
 }
 
 pylintScore() {
-    py_files=$1
-    target_score=$2
+    py_files=$2
+    target_score=$1
     score=$(pylint $py_files | sed -n -e 's/^Your code has been rated at \([0-9]\{1,2\}\.[0-9]\{1,2\}\).*/\1/p')
     is_ok=$(greaterThan $score $target_score)
 
