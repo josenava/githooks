761a84226b9142c5de4ae80ff7ba65e6ed5943aa
Jose Navarrete <janavarretecristino@gmail.com>
2020-04-20 11:33:05 +0200

    Add python-pylint.sh

---
+++ README.md
@@ -0,0 +1,7 @@
+# Githooks / Shell scripts
+
+This repository contains some githooks or shell scripts that I wrote to fit my
+needs.
+
+List included here:
+- python-pylint.sh
---
+++ python-pylintscore.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Waiting till pylint --under-score gets released.
+
+greaterThan() {
+    # awk comes to rescue when using floats
+    awk -v n1="$1" -v n2="$2" 'BEGIN {print (n1>=n2)}'
+}
+
+pylintScore() {
+    py_files=$1
+    target_score=$2
+    score=$(pylint $py_files | sed -n -e 's/^Your code has been rated at \([0-9]\{1,2\}\.[0-9]\{1,2\}\).*/\1/p')
+    is_ok=$(greaterThan $score $target_score)
+
+    if [ "$is_ok" -eq "0" ]; then
+        echo "expected score: ${target_score}, got: ${score}"
+        return 1
+    else
+        echo "score ${score}"
+        return 0
+    fi
+}
